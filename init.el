ef969ff76d9f4981f5e8a1280fb0154efb283e8c
Nicolas Forgerit <hello@nicolasforgerit.com>
2023-08-06 05:48:12 +0300

    add: init.el file

---
+++ init.el
@@ -0,0 +1,31 @@
+(message "hello emacsworld")
+
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+;;(package-refresh-contents)
+
+;; pixel-based scroll with bezier scrolling dynamics
+(add-to-list 'load-path "/Users/nico/code/good-scroll.el")
+(require 'good-scroll)
+(good-scroll-mode 1)
+
+;; package.el
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; teach emacs proper text editing
+(evil-mode 1)
+
